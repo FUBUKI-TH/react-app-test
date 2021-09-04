1c5f48d658090cf64c77f3b84e540484964457ce
FUBUKI-TH <63845392+FUBUKI-TH@users.noreply.github.com>
2021-09-04 10:14:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello ICT314
-         
+         Hello ICT314 
         </p>
         <a
           className="App-link"
